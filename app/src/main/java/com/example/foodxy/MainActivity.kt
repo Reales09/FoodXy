9ef78e62d2051bf13fcb314d8b198547a6fd0636
rclar <rclarence123@gmail.com>
2021-11-05 00:26:25 -0500

    Primer commit programacion

--- app/src/main/java/com/example/foodxy/MainActivity.kt
+++ app/src/main/java/com/example/foodxy/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.example.foodxy
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = FirebaseFirestore.getInstance()
+
+        //Consultar información
+
+
+        db.collection("ciudades").document("NY").addSnapshotListener { value, error ->
+
+            value?.let { document ->
+                if (document != null) {
+
+                    val ciudad = document.toObject(Ciudad::class.java)
+
+                    Log.d("Firebase", "DocumentSnapshot data: ${document.data}")
+
+                    Log.d("Firebase", "Population: ${ciudad?.population}")
+                    Log.d("Firebase", "color: ${ciudad?.color}")
+                    Log.d("Firebase", "postal code: ${ciudad?.pc}")
+
+                } else {
+                    Log.d("Firebase", "No such document")
+                }
+            }
+
+            //Ingresar información
+
+            db.collection("ciudades").document("LA").set(Ciudad(3000000, "red"))
+                .addOnSuccessListener {
+
+                    Log.d("Firebase", "Se guardo la ciudad correctamente")
+                }.addOnFailureListener { exception ->
+
+                Log.d("FirebaseError", "Error ingresar dato", exception)
+            }
+
+        }
     }
 }
+
+data class Ciudad (val population: Int=0, val color: String="",val pc: Int=0)
