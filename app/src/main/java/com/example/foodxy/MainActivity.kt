9e15097354444b2547eabb437e1a644d23180abc
rclar <rclarence123@gmail.com>
2021-11-07 12:14:55 -0500

    Estucturando projecto con MVVM

--- app/src/main/java/com/example/foodxy/MainActivity.kt
+++ app/src/main/java/com/example/foodxy/MainActivity.kt
@@ -1,54 +1,28 @@
 package com.example.foodxy
 
-import android.content.ContentValues.TAG
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayOutputStream
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val db = FirebaseFirestore.getInstance()
 
-        //Consultar información
-
-
-        db.collection("ciudades").document("NY").addSnapshotListener { value, error ->
-
-            value?.let { document ->
-                if (document != null) {
-
-                    val ciudad = document.toObject(Ciudad::class.java)
-
-                    Log.d("Firebase", "DocumentSnapshot data: ${document.data}")
-
-                    Log.d("Firebase", "Population: ${ciudad?.population}")
-                    Log.d("Firebase", "color: ${ciudad?.color}")
-                    Log.d("Firebase", "postal code: ${ciudad?.pc}")
-
-                } else {
-                    Log.d("Firebase", "No such document")
-                }
-            }
-
-            //Ingresar información
-
-            db.collection("ciudades").document("LA").set(Ciudad(3000000, "red"))
-                .addOnSuccessListener {
-
-                    Log.d("Firebase", "Se guardo la ciudad correctamente")
-                }.addOnFailureListener { exception ->
-
-                Log.d("FirebaseError", "Error ingresar dato", exception)
-            }
-
-        }
     }
 }
-
-data class Ciudad (val population: Int=0, val color: String="",val pc: Int=0)
---
+++ app/src/main/java/com/example/foodxy/data/Post.kt
@@ -0,0 +1,4 @@
+package com.example.foodxy.data
+
+class Post {
+}
