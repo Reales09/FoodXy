74ee02ca875bbede9ff83ec07a905f431ea29ba2
rclar <rclarence123@gmail.com>
2021-11-07 12:19:48 -0500

    Creando modelo

---
+++ app/src/main/java/com/example/foodxy/data/model/Post.kt
@@ -0,0 +1,10 @@
+package com.example.foodxy.data.model
+
+import com.google.firebase.Timestamp
+
+data class Post (val profile_pictures: String ="",
+                 val profile_name: String = "",
+                 val post_timestamp: Timestamp?=null,
+                 val post_image: String = ""
+
+)
