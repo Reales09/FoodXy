62053568cce7d618dd33ef6e6b4f807aaec8ab88
rclar <rclarence123@gmail.com>
2021-11-09 17:05:35 -0500

    ocultando nav de login y register

---
+++ app/src/main/java/com/example/foodxy/core/viewextensions.kt
@@ -0,0 +1,12 @@
+package com.example.foodxy.core
+
+import android.view.View
+
+fun View.hide(){
+    this.visibility = View.GONE
+
+}
+
+fun View.show(){
+    this.visibility = View.VISIBLE
+}
--- app/src/main/java/com/example/foodxy/ui/MainActivity.kt
+++ app/src/main/java/com/example/foodxy/ui/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.foodxy.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.foodxy.R
+import com.example.foodxy.core.hide
+import com.example.foodxy.core.show
 import com.example.foodxy.databinding.ActivityMainBinding
 
 
@@ -13,14 +17,43 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
 
         binding.bottomNavigationView.setupWithNavController(navController)
+
+        observeDestinationChange()
+
+
     }
+
+    private fun observeDestinationChange(){
+
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+
+            when(destination.id){
+
+                R.id.loginFragment -> {
+
+                    binding.bottomNavigationView.hide()
+                }
+
+                R.id.registerFragment -> {
+                    binding.bottomNavigationView.hide()
+                }
+                else ->{
+                    binding.bottomNavigationView.show()
+                }
+            }
+        }
+
+    }
+
 }
