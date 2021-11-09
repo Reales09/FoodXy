773017b65a11711766f113e8d099056637f84e47
rclar <rclarence123@gmail.com>
2021-11-09 16:29:15 -0500

    Creando menu de navegacion

--- app/src/main/java/com/example/foodxy/ui/MainActivity.kt
+++ app/src/main/java/com/example/foodxy/ui/MainActivity.kt
@@ -2,16 +2,25 @@ package com.example.foodxy.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.foodxy.R
+import com.example.foodxy.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
 
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/example/foodxy/ui/camera/CameraFragment.kt
@@ -0,0 +1,16 @@
+package com.example.foodxy.ui.camera
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.foodxy.R
+
+class CameraFragment : Fragment(R.layout.fragment_camera) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
