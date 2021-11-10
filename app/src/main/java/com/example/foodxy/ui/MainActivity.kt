7ba65539560ba93a068c2ff749771cfb3fb2ca05
rclar <rclarence123@gmail.com>
2021-11-10 00:54:08 -0500

    Crear splash e icono de la app, configuración tema modo claro y oscuro

--- app/src/main/java/com/example/foodxy/ui/MainActivity.kt
+++ app/src/main/java/com/example/foodxy/ui/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_FoodXy)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
