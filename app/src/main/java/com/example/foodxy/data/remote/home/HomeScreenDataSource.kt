278d1010e8a4da9e8e5819eb44583c187139aa72
rclar <rclarence123@gmail.com>
2021-11-13 09:37:49 -0500

    Ordenando post

--- app/src/main/java/com/example/foodxy/data/remote/home/HomeScreenDataSource.kt
+++ app/src/main/java/com/example/foodxy/data/remote/home/HomeScreenDataSource.kt
@@ -4,6 +4,7 @@ import com.example.foodxy.core.Result
 import com.example.foodxy.data.model.Post
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import kotlinx.coroutines.tasks.await
 
 
@@ -12,7 +13,7 @@ class HomeScreenDataSource {
 suspend fun getLatestPost(): Result<List<Post>> {
 
         val postList = mutableListOf<Post>()
-        val querySnapshot = FirebaseFirestore.getInstance().collection("post").get().await()
+        val querySnapshot = FirebaseFirestore.getInstance().collection("post").orderBy("created_at", Query.Direction.DESCENDING).get().await()
         for (post in querySnapshot.documents) {
             post.toObject(Post::class.java) ?. let {fbPost ->
                 fbPost.apply { created_at= post.getTimestamp("created_at",DocumentSnapshot.ServerTimestampBehavior.ESTIMATE)?.toDate() }
